6415262425b64540ea659e019cad72a1099c83be
Leandro <leandrosilvadeoliveira0605@gmail.com>
2024-09-22 12:51:48 -0300

    dont repeat response working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Perguntas que serão utilizadas no Quiz 
 const quiz = [
     {
         question: "Quanto é 1 + 1?",
@@ -28,42 +30,125 @@ const quiz = [
     },
 ];
 
+// Elemento do HTML
 const answerTitle = document.getElementById("answer-title");
 const nextButton = document.getElementById("next-button");
 
 const pointsScreen = document.getElementById("points-area");
 const answerList = Array.from(document.getElementsByTagName("li"));
 
+// Pontos Adquiridos
 let points = 0;
+
+// Pergunta atual
 let currentId = 0;
 
 
+// atualização as Perguntas
 const updateQuestion = () => {
+
+    // Tamanho do Quiz é menor que a pergunta atual?
+    if (quiz.length < currentId) {
+        console.log('Você finalizou o Quiz!');
+        return;
+    }
+
+    // Pegando o titulo da Questão, com base no currentID;
     answerTitle.textContent = quiz[currentId].question;
 
+    // Percorrendo todos os itens de AnswerList ( li )
     answerList.forEach((item, i) => {
+
+        // Adicionando as respostas no LI
+        item.style.color = '#C6ADFF'
         item.textContent = quiz[currentId].questions[i].text; // Ok
+
+
     });
 };
 
-updateQuestion();
-
+// Próxima Questão ( Geralmente no Botão )
 const nextQuestion = () => {
 
+    // Tamanho do Quiz é menor ou igual a pergunta atual?
     if (quiz.length <= currentId) {
 
         alert("Você está no final!");
-        return;
+        return; // Para o código
 
     } else {
 
+        // Aumenta +1 na pergunta atual
         currentId++;
+        // Atualiza a questão chamando a função
         updateQuestion();
 
-        console.log(`Tamanho: ${quiz.length}`);
-        console.log(`Id: ${currentId}`);
-        console.log(quiz[currentId]);
+        // Debug para controle
+        console.log(`Id: ${currentId}\n`);
+        console.log(`Questão Atual: ${currentId}`);
+        console.log(`Tamanho do Quiz: ${quiz.length}`);
     }
+
+    // Remove a class 'Respondida' dos itens!
+    answerList.forEach(answer => answer.classList.remove('respondida'));
 };
+
+const correctAnswer = () => {
+    answerList.forEach((item, i) => {
+        item.addEventListener('click', () => {
+
+            if (item.classList.contains('respondida')) {
+                return;
+            }
+
+            if (currentId > quiz.length) {
+                console.log('Você finalizou o Quiz!');
+                return;
+            }
+
+            const clickedAnswer = quiz[currentId].questions[i].isCorrect;
+
+            if (clickedAnswer) {
+
+                // adiciona 15 pontos
+                updatePoints(15);
+                // Altera a cor para Verde Claro
+                item.style.color = 'lightGreen';
+
+                // Adicionando a classe em todos os itens, para não ter novas respostas após acertas!
+                answerList.forEach(answer => answer.classList.add('respondida'));
+
+            } else {
+
+                // remove 5 pontos
+                updatePoints(-5);
+
+                // Altera a cor para Vermelho
+                item.style.color = 'red';
+            }
+
+            // Adiciona a classe Respondida no item clicado
+            item.classList.add('respondida');
+
+        })
+
+    });
+}
+
+const updatePoints = (values) => {
+
+    // Soma os pontos com o values fornecido
+    points = points + values;
+
+    // Adiciona os pontos na tela juntamente com o texto
+    pointsScreen.textContent = `Pontução: ${points}`;
+
+}
+
+// Chamando o evento para o Quiz iniciar quando a página for carregada
+updateQuestion();
+
+// Chamando a função responsável por permitir a interação com os itens
+correctAnswer();
 
 nextButton.addEventListener("click", nextQuestion);
